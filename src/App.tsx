eb7c396c48641d64f520495d1792536eb3852994
collin.charvat <collin.charvat@keithley.com>
2022-12-16 21:13:30 -0500

    First_commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
+
+
+const toggleDrawer =
+  (open: boolean) =>
+    (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event.type === 'keydown' &&
+        ((event as React.KeyboardEvent).key === 'Tab' ||
+          (event as React.KeyboardEvent).key === 'Shift')
+      ) {
+        return;
+      }
+    };
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <Button onClick={toggleDrawer(true)}>{"left"}</Button>
+        <Drawer
+          anchor={"left"}
+          open={true}
+          onClose={toggleDrawer(false)}
         >
-          Learn React
-        </a>
+        </Drawer>
       </header>
     </div>
   );
-}
-
+};
 export default App;
