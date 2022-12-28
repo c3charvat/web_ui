1401d26e73588bce4ffcfc35ea773e820dc56b7c
c3charvat <c3charvat@gmail.com>
2022-12-27 19:42:59 -0500

    Add_switch_images

---
+++ src/UIComponents/RegularSwitch.tsx
@@ -0,0 +1,98 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch, { SwitchProps } from '@mui/material/Switch';
+import { ColorModeContext } from '../color-context';
+import { useTheme } from '@mui/material/styles';
+import gearimage from "./gearbox.png";
+import liveimage from "./live_signal.png"
+
+const RegularSwitch = styled(Switch)(({ theme }) => ({
+  width: 62,
+  height: 34,
+  padding: 7,
+  '& .MuiSwitch-switchBase': {
+    margin: 1,
+    padding: 0,
+    transform: 'translateX(6px)',
+    '&.Mui-checked': {
+      color: '#fff',
+      transform: 'translateX(22px)',
+      '& .MuiSwitch-thumb:before': {
+        backgroundImage: `url(${gearimage})`,
+      },
+      '& + .MuiSwitch-track': {
+        opacity: 1,
+        backgroundColor: theme.palette.mode === 'dark' ? '#8796A5' : '#aab4be',
+      },
+    },
+  },
+  '& .MuiSwitch-thumb': {
+    backgroundColor: theme.palette.mode === 'dark' ? '#003892' : '#001e3c',
+    width: 32,
+    height: 32,
+    '&:before': {
+      content: "''",
+      position: 'absolute',
+      width: '100%',
+      height: '100%',
+      left: 0,
+      top: 0,
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center',
+      backgroundImage: `url(${liveimage})`
+    },
+  },
+  '& .MuiSwitch-track': {
+    opacity: 1,
+    backgroundColor: theme.palette.mode === 'dark' ? '#8796A5' : '#aab4be',
+    borderRadius: 20 / 2,
+  },
+}));
+
+
+
+export default function StyledSwitch(props:any) {
+  const theme = useTheme();
+  const colorMode = React.useContext(ColorModeContext);
+  return (
+    <FormGroup>
+    <FormControlLabel control={<RegularSwitch sx={{ m: 1 }}
+        defaultChecked />} label={props.label} />
+  </FormGroup>
+  );
+}
+
+// I know i need somthing like the code below but im a little stummped how to bring the 
+// color swithcing part in 
+
+// export default function ToggleColorMode() {
+//   const [mode, setMode] = React.useState<'light' | 'dark'>('light');
+//   const colorMode = React.useMemo(
+//     () => ({
+//       toggleColorMode: () => {
+//         setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+//       },
+//     }),
+//     [],
+//   );
+
+//   const theme = React.useMemo(
+//     () =>
+//       createTheme({
+//         palette: {
+//           mode,
+//         },
+//       }),
+//     [mode],
+//   );
+
+//   return (
+//     <ColorModeContext.Provider value={colorMode}>
+//       <ThemeProvider theme={theme}>
+//         <MyApp />
+//       </ThemeProvider>
+//     </ColorModeContext.Provider>
+//   );
+// }
