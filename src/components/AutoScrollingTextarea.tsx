54c9fdbf936c87d32102e8c301251c7250907f49
c3charvat <c3charvat@gmail.com>
2022-12-30 22:21:27 -0500

    Remove_class

--- src/components/AutoScrollingTextarea.tsx
+++ src/components/AutoScrollingTextarea.tsx
@@ -1,36 +1,17 @@
-/*
-Copyright 2017 Peter Holak
+import React, { useEffect } from "react";
+import { styled } from '@mui/material/styles';
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+export const AutoScrollingTextArea: React.FC<
+  React.TextareaHTMLAttributes<HTMLTextAreaElement> & { autoScroll?: boolean }
+> = (props) => {
+  const element = React.useRef<HTMLTextAreaElement>(null);
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
+  useEffect(() => {
+    if (!element.current || props.autoScroll === false) return;
+    element.current.scrollTop = element.current.scrollHeight;
+  }, [props.autoScroll]);
 
-import * as React from 'react'
+  return <textarea {...props} style={{...props.style}} ref={element} />;
+};
 
-type TextareaProps = React.TextareaHTMLAttributes<HTMLTextAreaElement>
-class AutoScrollingTextarea extends React.Component<TextareaProps & { autoScroll?: boolean }, {}> {
-
-    element!: HTMLTextAreaElement|null
-
-    componentDidMount() {
-        if (this.element && this.props.autoScroll !== false) {
-            this.element.scrollTop = this.element.scrollHeight
-        }
-    }
-
-    componentDidUpdate(prevProps: TextareaProps, prevState: {}) {
-        if (prevProps.value !== this.props.value && this.element && this.props.autoScroll !== false) {
-            this.element.scrollTop = this.element.scrollHeight
-        }
-    }
-
-    render() {
-        return <textarea {...this.props} ref={e => this.element = e} />
-    }
-  
-}
-  
-export default AutoScrollingTextarea
