df2ded6f783b24fb62963c259c955454f27a1783
c3charvat <c3charvat@gmail.com>
2023-01-08 15:31:08 -0500

    convert_back_to typescript

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import {atom, useRecoilState} from 'recoil';
+import {
+  ThemeProvider,
+  createTheme,
+  responsiveFontSizes,
+} from "@mui/material/styles";
+import { deepmerge } from "@mui/utils";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { getDesignTokens, getThemedComponents } from "./Theme";
+import { ColorModeContext } from "./contexts/color-context";
+import DrawerLeft from "./components/Drawer";
+
+export default function App() {
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+  const [mode, setMode] = React.useState<'light' | 'dark'>('light');
+
+  React.useEffect(() => {
+    setMode(prefersDarkMode ? "dark" : "light");
+  }, [prefersDarkMode]);
+
+  const colorMode = React.useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
+      },
+    }),
+    []
+  );
+
+  let theme = React.useMemo(
+    () =>
+      createTheme(deepmerge(getDesignTokens(mode), getThemedComponents(mode))),
+    [mode]
+  );
+
+  theme = responsiveFontSizes(theme);
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <DrawerLeft></DrawerLeft>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+}
