c65a73f6e2aec4385f0891a265702caf4eecb7ca
c3charvat <c3charvat@gmail.com>
2023-01-21 20:03:29 -0500

    WSU_Logo

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,16 +1,16 @@
 import * as React from 'react';
 import { Typography } from '@mui/material';
 import SliderGroup from './SliderUI';
+import SliderGroupV2 from './SliderV2';
 import TableGroup from './TableUI';
 import ConsoleGroup from './ConsoleUI';
-
-
+var logo = require('../img/green_logo.png');
 
 export default function MainContent() {
     return (
         <React.Fragment>
             <Typography paragraph>
-                <SliderGroup></SliderGroup>
+                <SliderGroupV2></SliderGroupV2>
             </Typography>
             <Typography paragraph>
                 <TableGroup></TableGroup>
@@ -27,6 +27,9 @@ export default function MainContent() {
             <Typography align='center' fontSize={15}>
             Acknowledgements: Daniel A. and Kyle R. for their endless support.
             </Typography>
+            <center>
+            <img src={logo} height={'25px'}></img>
+            </center>
         </React.Fragment>
 
     );
